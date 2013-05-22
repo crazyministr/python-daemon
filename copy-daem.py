15e4b30eccad4b245ed4abd57df3113529bc3261
crazyministr <crazyministr@gmail.com>
2013-05-22 20:28:47 +0400

    copy-daem.py v 1.0

--- copy-daem.py
+++ copy-daem.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python
-"""
-example for daemon launch in the system
-1) apt-get install python-daemon
-2) touch file /var/run/copy-daemon.pid and edit it any random number and save it
-3) launch this script ./copy-daem.py start/stop in console and close console without exit from session
-"""
-
-"""
-    1. Должен быть создан файл .netrc, c логином и паролем от git'a
-"""
-
 import os, time, subprocess, sys
 from daemon import runner
-# import git, ConfigParser
 
 class Daemon():
     def __init__(self):
@@ -25,13 +13,24 @@ class Daemon():
         self.git_pull = '/var/www/rep/press/'
         self.main_dir = '/var/www/press/'
 
-    def copy(self):
+    def gitPull(self):
         os.chdir(self.git_pull)
         try:
             p = subprocess.Popen("git pull origin test", shell = True)
             p.wait()
         except Exception, e:
             sys.stderr.write("git pull failed: %d (%s)\n" % (e.errno, e.strerror))
+
+    def copy(self):
+        os.chdir(self.main_dir)
+        p = subprocess.Popen("rm -r apps/", shell = True)
+        p.wait()
+        p = subprocess.Popen("rm -r templates/", shell = True)
+        p.wait()
+        p = subprocess.Popen("cp -r " + self.git_pull + "apps/" + " " + self.main_dir, shell = True)
+        p.wait()
+        p = subprocess.Popen("cp -r " + self.git_pull + "templates/" + " " + self.main_dir, shell = True)
+        p.wait()
 
     def run(self):
         os.chdir(self.git_pull)
@@ -42,14 +41,15 @@ class Daemon():
             sys.stderr.write("git checkout failed: %d (%s)\n" % (e.errno, e.strerror))
 
         while True:
+            self.gitPull()
             self.copy()
+            # p = subprocess.Popen('python manage.py collectstatic', shell = True)
+            # p.wait()
             # p = subprocess.Popen('/etc/init.d/gunicorn restart', shell = True)
             # p.wait()
             # p = subprocess.Popen('/etc/init.d/celeryd restart', shell = True)
             # p.wait()
             # p = subprocess.Popen('/etc/init.d/nginx restart', shell = True)
-            # p.wait()
-            # p = subprocess.Popen('python manage.py collectstatic')
             # p.wait()
             time.sleep(60) # 1 minute
 
